5db49090432267c8f0ab60e9e13c15effbcd7375
Nico Diaz <ndiaz@forcegrid.com>
2014-05-12 20:25:22 -0300

    Caso de uso: Recuperar XML
    Modificacion: Se agrega el jquery necesario para recuperar una URL y
    cargar el textarea con la informacion

---
+++ WebContent/js/init.js
@@ -0,0 +1,15 @@
+/**
+ * Common Initializations
+ */
+$(function() {
+
+	$("select option:eq(0)").attr("disabled", "disabled");
+
+	$('#systemselect').change(function() {
+
+		$.get("/agea_test/index.action", function(data) {
+			$("#info").val(data);
+		});
+	});
+
+});
